0096e6225f03d099b948f3fc6f785e6eea409344
KODZO A. M. Mawuena <nashmichel70@gmail.com>
2021-11-15 00:59:00 +0000

    Some Modifications

---
+++ core/static/core/home.js
@@ -0,0 +1,44 @@
+/* Donor Animation section */
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	var carousel = function() {
+		$('.featured-carousel').owlCarousel({
+	    loop: true,
+	    autoplay: true,
+	    margin:30,
+	    animateOut: 'fadeOut',
+	    animateIn: 'fadeIn',
+	    dots: true,
+        nav: true,
+	    autoplayHoverPause: false,
+	    items: 1,
+	    navText : ["<span class='ion-ios-arrow-back'></span>","<span class='ion-ios-arrow-forward'></span>"],
+	    responsive:{
+	      0:{
+	        items:1
+	      },
+	      600:{
+	        items:2
+	      },
+	      1000:{
+	        items:3
+	      }
+	    }
+		});
+
+	};
+	carousel();
+
+})(jQuery);
